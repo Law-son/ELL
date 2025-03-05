0d26391c29635b30770bca16d732420bd61923e3
Kingsford-Q <158656114+Kingsford-Q@users.noreply.github.com>
2025-03-05 00:05:16 +0000

    Changes

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -292,7 +292,7 @@ export class Homepage extends Component {
                 </div>
             </div>
 
-            <div className = "lg:mt-[530px] md:mt-[1450px] mt-[1650px] w-full relative flex justify-center">
+            <div className = "lg:mt-[530px] md:mt-[1450px] mt-[1470px] w-full relative flex justify-center">
                 <div className = "lg:flex justify-center items-center lg:w-[60%] w-[70%] gap-5">
                     <div className = "">
                         <div className = "text-[27px] font-[700] text-[#353535FF] lg:text-left ">Our Clients Say
